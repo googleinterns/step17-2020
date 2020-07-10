360746713a4b612a9dc6803e917518ba3101e00a
Tyler Hernandez <tylerh6201@gmail.com>
2020-07-10 19:31:57 +0000

    functional reviews under user page display

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -7,10 +7,8 @@ function determineLogin() {
     form.action='/login';
     const button = document.createElement('button');
     if (!isLoggedIn) {
-      textElement.innerText = "Please log in to view this page";
       button.innerHTML = "Login";
     } else {
-      textElement.innerText = "You're logged in!";
       displayEmail();
       button.innerHTML = "Logout";
     }
@@ -25,8 +23,37 @@ function determineLogin() {
 function displayEmail() {
   fetch('/get-email').then(response => response.json()).then((EMAIL) => {
     document.getElementById('login-container').innerText = EMAIL;
+    loadComments(EMAIL);
   }).catch(error => {
     console.error('There has been a problem with your operation:', error);
   });
 }
 
+function loadComments(email) {
+  var url = new URL('/comment', "https://" + window.location.hostname);
+  var params = {email: email};
+  url.search = new URLSearchParams(params).toString();
+  fetch(url).then(response => response.json()).then((drinks) => {
+    const ratingListElement = document.getElementById('list');
+    ratingListElement.innerHTML = "";
+
+    drinks.forEach((drink) => 
+    {
+      ratingListElement.appendChild(createListElement(drink));
+    })
+
+  });
+}
+
+/** Creates an <li> element containing text. */
+function createListElement(drink) {
+  console.log(drink);
+  const ratingElement = document.createElement('li');
+  ratingElement.className = 'drink';
+
+  const drinkElement = document.createElement('span');
+  drinkElement.innerText = drink.drink + ", " + drink.rating + "/5, " + drink.content;
+
+  ratingElement.appendChild(drinkElement);
+  return ratingElement;
+}
