07cc235a9324475765a1ca7ffd4444fbf2f16c4f
yijiazhang <yijiazhang@google.com>
2020-07-22 18:04:47 +0000

    Add clearing previous search result functionality

--- src/main/webapp/map.js
+++ src/main/webapp/map.js
@@ -3,6 +3,7 @@ var infoWindow;
 var shopInfo;
 var userPos;
 var geocoder;
+var markers = [];
 
 /** Creates a map that shows all coffee shops around the user. */
 function createMap() {
@@ -10,7 +11,7 @@ function createMap() {
   var newyork = new google.maps.LatLng(40.7128, -74.0060);
   map = new google.maps.Map(
     document.getElementById('map'),
-    {center: newyork, zoom: 14});
+    {center: newyork, zoom: 13});
   
   // Infowindow for to handle error in get user location
   infoWindow = new google.maps.InfoWindow();
@@ -27,6 +28,7 @@ function createMap() {
         };
         map.setCenter(userPos);
         coffeeShopRequest(userPos);
+        // displayRoute(userPos);
       },
       function() {
         handleLocationError(true, infoWindow, map.getCenter());
@@ -42,7 +44,7 @@ function createMap() {
 function coffeeShopRequest(userPos) {
 	var request = {
     location: userPos,
-    radius: '500',
+    radius: '300',
     query: 'coffee shop'
   };
   service = new google.maps.places.PlacesService(map);
@@ -52,18 +54,19 @@ function coffeeShopRequest(userPos) {
 function handleLocationError(browserHasGeolocation, infoWindow, userPos) {
   infoWindow.setPosition(userPos);
   if (browserHasGeolocation) {
-  	infoWindow.setContent("Error: The Geolocation service failed.");
+  	infoWindow.setContent("The Geolocation service failed. Please enter a zipcode to view nearby coffee shops.");
   } else {
-  	infoWindow.setContent("Error: Your browser doesn't support geolocation.");
+  	infoWindow.setContent("Your browser doesn't support geolocation. Please enter a zipcode to view nearby coffee shops.");
   }
   infoWindow.open(map);
 }
 
 // Call this wherever needed to actually handle the display
 function codeAddress() {
+	// Clear markers from previous search results
+	clearMarkers();
 	geocoder = new google.maps.Geocoder();
 	var zipCode = document.getElementById("zipcode").value;
-  console.log(zipCode);
     geocoder.geocode( {
       componentRestrictions: {
         country: 'US',
@@ -87,8 +90,9 @@ function codeAddress() {
  function callback(results, status) {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
-      createMarker(results[i]);
+        createMarker(results[i]);
     }
+    displayMarkers();
   }
 }
 
@@ -107,4 +111,18 @@ function createMarker(place) {
     localStorage.setItem("store", place.place_id);
     shopInfo.open(map, marker);
   });
+  markers.push(marker);
+}
+
+function displayMarkers() {
+  for (var i = 0; i < markers.length; i++ ) {
+    markers[i].setMap(map);
+  }
+}
+
+function clearMarkers() {
+  for (var i = 0; i < markers.length; i++ ) {
+    markers[i].setMap(null);
+  }
+  markers = [];
 }
