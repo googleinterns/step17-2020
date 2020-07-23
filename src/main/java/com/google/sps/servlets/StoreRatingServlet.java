b7caa671ebc0aff10d84927258a5206031aed00d
Deron <deron.gentles@yahoo.com>
2020-07-23 21:54:19 +0000

    remove log functions

--- src/main/java/com/google/sps/servlets/StoreRatingServlet.java
+++ src/main/java/com/google/sps/servlets/StoreRatingServlet.java
@@ -26,14 +26,14 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/store-rating")
 public class StoreRatingServlet extends HttpServlet {
 
-  public Map<String, Integer> votes = createMap();
+  public Map<String, Integer> ratingsToVotes = createMap();
 
   private Map<String, Integer> createMap() {
     Map<String, Integer> map = new LinkedHashMap<>();
 
     // This piece of code creates five keys corresponding to ratings 1-5 and then
     // maps 0 to those keys indicating that no one has voted for those ratings yet.
-    for (int i = 0; i <= 5; i++) map.put(Integer.toString(i), 0);
+    for (int i = 1; i <= 5; i++) map.put(Integer.toString(i), 0);
     return map;
   }
 
@@ -41,15 +41,15 @@ public class StoreRatingServlet extends HttpServlet {
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("application/json");
     Gson gson = new Gson();
-    String json = gson.toJson(votes);
+    String json = gson.toJson(ratingsToVotes);
     response.getWriter().println(json);
   }
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     String rating = request.getParameter("store-rating");
-    int currentVotes = votes.containsKey(rating) ? votes.get(rating) : 0;
-    votes.put(rating, currentVotes + 1);
+    int currentVotes = ratingsToVotes.get(rating);
+    ratingsToVotes.put(rating, currentVotes + 1);
 
     response.sendRedirect("coffeeshop.html");
   }
